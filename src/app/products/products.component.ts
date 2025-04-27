8d06ba87b372f27ba8784a223a6a48787458b748
jihade1997 <zidani.jihade@gmail.com>
2025-04-27 11:43:19 +0100

    commit delete product from the list + add field check

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -8,12 +8,20 @@ import { RouterLink } from '@angular/router';
   styleUrl: './products.component.scss',
   standalone: true
 })
-export class ProductsComponent {
-products = [
-  {id:1,name:"Computer",price:2300,selected: true},
-  {id:2,name:"Printer",price:1200,selected: false},
-  {id:3,name:"Smart Phone",price:1100,selected: true}
+export class ProductsComponent implements OnInit {
+handleDelete(product : any): void {
 
+this.products = this.products.filter((p : any): boolean => p.id != product.id);}
+  products : any;
+  ngOnInit(): void {
+    this.products = [
+      {id:1,name:"Computer",price:2300,selected: true},
+      {id:2,name:"Printer",price:1200,selected: false},
+      {id:3,name:"Smart Phone",price:1100,selected: true}
+    ]
+  }
+  constructor(){
 
-]
+  }
+
 }
