066505a138b20197af3288a595ac2c22590f2f7e
jihade1997 <zidani.jihade@gmail.com>
2025-04-27 10:48:15 +0100

    front end project using angular 19

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+  standalone: true
+})
+export class HomeComponent {
+
+}
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-products',
+  imports: [],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.scss',
+  standalone: true
+})
+export class ProductsComponent {
+products = [
+  {id:1,name:"Computer",price:2300,selected: true},
+  {id:2,name:"Printer",price:1200,selected: false},
+  {id:3,name:"Smart Phone",price:1100,selected: true}
+
+
+]
+}
